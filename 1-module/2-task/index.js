69b58ede3f16e3966445c1efaca1a4dc2e799648
Dmitriy Tarasov <dmitriystarasov@ya.ru>
2022-01-29 13:14:40 +0300

    editing

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -3,15 +3,12 @@ function print(text) {
 }
 
 function isValid(name) {
-HEAD
+
   if (name && !name.includes(' ') && name.length >= 4) {
     return true;
   } else {
     return false;
 }
-
-  return !!name && !name.includes(' ') && name.length >= 4;
-c3603b6dea5f24e49af6f1f0eb7b7b8f8daf2e2c
 }
 
 function sayHello() {
--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,16 @@
 function sumSalary(salaries) {
-  // ваш код...
+
+  let sum = 0
+  for (let salary of Object.values(salaries)) {
+    sum += salary;
+  }
+  return sum;
 }
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300
+};
+
+//alert( sumSalary(salaries));
